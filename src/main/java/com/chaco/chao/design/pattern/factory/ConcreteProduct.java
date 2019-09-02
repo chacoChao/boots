ed8bffe1814b478ef8645676a3dce2b5aa74b94a
zhaopeiyan <zhaopeiyan001@ke.com>
2019-09-02 10:18:38 +0800

    factory demo2

--- src/main/java/com/chaco/chao/design/pattern/factory/ConcreteProduct.java
+++ src/main/java/com/chaco/chao/design/pattern/factory/ConcreteProduct.java
@@ -1,8 +1,24 @@
 package com.chaco.chao.design.pattern.factory;
 
+import java.text.DateFormat;
+
 /**
  * author:zhaopeiyan001
  * Date:2019-08-29 15:35
  */
-public class ConcreteProduct {
+public class ConcreteProduct implements Product {
+
+    public ConcreteProduct() {
+
+    }
+
+    /**
+     * 静态工厂方法
+     * @return
+     */
+    public static ConcreteProduct factory() {
+        DateFormat dateInstance = DateFormat.getDateInstance();
+        DateFormat.getInstance();
+        return new ConcreteProduct();
+    }
 }
--- src/main/java/com/chaco/chao/design/pattern/factory/Creator.java
+++ src/main/java/com/chaco/chao/design/pattern/factory/Creator.java
@@ -5,4 +5,12 @@ package com.chaco.chao.design.pattern.factory;
  * Date:2019-08-29 15:34
  */
 public class Creator {
+
+    /**
+     * 静态工厂方法
+     * @return
+     */
+    public static Product factory() {
+        return new ConcreteProduct();
+    }
 }
