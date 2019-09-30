279fd6ba4e87fa40a50c10a1757a2b2b29b83c1d
zhaopeiyan <zhaopeiyan001@ke.com>
2019-09-30 18:03:29 +0800

    google aviator

--- src/test/java/com/chaco/chao/advitorTest/advitorTest.java
+++ src/test/java/com/chaco/chao/advitorTest/advitorTest.java
@@ -1,8 +1,21 @@
 package com.chaco.chao.advitorTest;
 
+import com.googlecode.aviator.AviatorEvaluator;
+import com.googlecode.aviator.AviatorEvaluatorInstance;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
 /**
  * author:zhaopeiyan001
  * Date:2019-09-27 14:44
  */
+@SpringBootTest
+@Slf4j
 public class advitorTest {
+    @Test
+    public void test() {
+        AviatorEvaluatorInstance aviatorEvaluatorInstance = AviatorEvaluator.newInstance();
+
+    }
 }
